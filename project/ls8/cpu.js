9a6457f6af135e0c088f4a0d39e8b6bf152b2fcf
perryahern <pahern@gmail.com>
2018-03-29 18:58:07 -0400

    added PRA, JMP, ST, working on interrupts now

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -18,6 +18,7 @@ class CPU {
         // Special-purpose registers
         this.reg.PC = 0; // Program Counter
         this.reg[7] = 0xF4; // Stack Pointer
+        this.reg[6] = 0;
     };
 	
     /**
@@ -31,10 +32,12 @@ class CPU {
      * Starts the clock ticking on the CPU
      */
     startClock() {
-        const _this = this;
+        this.timer = setInterval(() => {
+            this.reg[6] = 0b00000001;
+        }, 1000);
 
         this.clock = setInterval(() => {
-            _this.tick();
+            this.tick();
         }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
     };
 
@@ -89,53 +92,99 @@ class CPU {
         // outlined in the LS-8 spec.
 
         // mnemonics for the instructions
-        const LDI = 0b10011001;
-        const PRN = 0b01000011;
-        const MUL = 0b10101010;
         const ADD = 0b10101000;
-        const PUSH = 0b01001101;
-        const POP = 0b01001100;
         const CALL = 0b01001000;
-        const RET = 0b00001001;
         const HLT = 0b00000001;
+        const JMP = 0b01010000;
+        const LDI = 0b10011001;
+        const MUL = 0b10101010;
+        const POP = 0b01001100;
+        const PRA = 0b01000010;
+        const PRN = 0b01000011;
+        const PUSH = 0b01001101;
+        const RET = 0b00001001;
+        const ST = 0b10011010;
 
-        const SP = 7;
+        // special variables
+        const SP = 7;   // Stack Pointer
+        const IS = 6;   // Interrupt Status
 
         // handlers for the functionality of each instruction
-        const handle_LDI = (register, value) => {
-            this.reg[register] = value;
-        };    
-        const handle_PRN = (register) => {
-            console.log(this.reg[register]);
-        };    
-        const handle_MUL = (registerA, registerB) => {
-            this.reg[registerA] = this.alu('MUL', registerA, registerB);
-        };
         const handle_ADD = (registerA, registerB) => {
             this.reg[registerA] = this.alu('ADD', registerA, registerB);
         };
-        const handle_PUSH = (register) => {
-            this.ram.write(--this.reg[SP], this.reg[register]);
-        };
-        const handle_PUSHval = (value) => {
-            this.ram.write(--this.reg[SP], value);
-        };
-        const handle_POPval = () => {
-            return this.ram.read(this.reg[SP]++);
-        };
-        const handle_POP = (register) => {
-            this.reg[register] = handle_POPval();
-        };
+        
         const handle_CALL = (register) => {
             handle_PUSHval(this.reg.PC + 2);
             this.reg.PC = this.reg[register];
         };
+        
+        const handle_HLT = () => this.stopClock();
+
+        const handle_JMP = (register) => {
+            this.reg.PC = this.reg[register];
+        };
+
+        const handle_LDI = (register, value) => {
+            this.reg[register] = value;
+        };
+
+        const handle_MUL = (registerA, registerB) => {
+            this.reg[registerA] = this.alu('MUL', registerA, registerB);
+        };
+        
+        const handle_POP = (register) => {
+            this.reg[register] = handle_POPval();
+        };
+        
+        const handle_POPval = () => {
+            return this.ram.read(this.reg[SP]++);
+        };
+
+        const handle_PRA = (register) => {
+            console.log(String.fromCharCode(this.reg[register]));
+        };
+
+        const handle_PRN = (register) => {
+            console.log(this.reg[register]);
+        };  
+
+        const handle_PUSH = (register) => {
+            this.ram.write(--this.reg[SP], this.reg[register]);
+        };
+
+        const handle_PUSHval = (value) => {
+            this.ram.write(--this.reg[SP], value);
+        };
+
         const handle_RET = () => {
             this.reg.PC = handle_POPval();
         };
-        const handle_HLT = () => this.stopClock();
 
-        // handler for invalid instruction
+        const handle_ST = (registerA, registerB) => {
+            this.ram.write(this.reg[registerA], this.reg[registerB]);
+        };
+
+        const interrupt_timer = () => {
+            console.log('Timer interrupt!');
+        };
+
+        // handler for interrupts
+        const handle_interrupt = (register) => {
+            const TIMER = 0b00000001;
+
+            // const interruptTable = {
+            //     [TIMER]: interrupt_timer,
+            // };
+            switch (register) {
+                case 0b00000001:
+                    console.log('Timer interrupt!');
+                    break;
+            };
+            this.reg[IS] = 0;
+        };
+
+        // handler for invalid instructions
         const handle_invalid_instruction = (instruction) => {
             console.log(`${instruction.toString(2)} is not a valid instruction; halting operation.`);
             handle_HLT();
@@ -143,15 +192,23 @@ class CPU {
 
         // branch table to pair mnemonics with functions
         const branchTable = {
-            [LDI]: handle_LDI,
-            [PRN]: handle_PRN,
-            [MUL]: handle_MUL,
             [ADD]: handle_ADD,
-            [PUSH]: handle_PUSH,
-            [POP]: handle_POP,
             [CALL]: handle_CALL,
-            [RET]: handle_RET,
             [HLT]: handle_HLT,
+            [JMP]: handle_JMP,
+            [LDI]: handle_LDI,
+            [MUL]: handle_MUL,
+            [POP]: handle_POP,
+            [PRA]: handle_PRA,
+            [PRN]: handle_PRN,
+            [PUSH]: handle_PUSH,
+            [RET]: handle_RET,
+            [ST]: handle_ST,
+        };
+
+        // check to see if there are any interrupts
+        if (this.reg[IS] !== 0) {
+            handle_interrupt(this.reg[IS]);
         };
 
         // call the function if it is in the branch table or handle invalid instruction
@@ -167,6 +224,7 @@ class CPU {
         // for any particular instruction.
         switch (IR) {
             case CALL:
+            case JMP:
             case RET:
               break;
             default:
